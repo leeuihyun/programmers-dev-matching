92f28a425cc21cd4f3c43fd481b54fd03f307f39
UihyunLee <88185154+leeuihyun@users.noreply.github.com>
2022-09-08 19:52:06 +0900

    Update util.js
    comma

--- dev-matching/SPA-ShoppingMall/src/util.js
+++ dev-matching/SPA-ShoppingMall/src/util.js
@@ -1,15 +1,10 @@
-export const getPrice = (price) => {
-    const strPrice = price.toString();
-    var count = 0;
-    for (var i = strPrice.length - 1; i >= 0; i--) {
-        var strChange = "";
-        count += 1;
-        if (count === 2) {
-            strChange = `${strPrice[i]},${strChange}`;
-            count = 0;
-        } else {
-            strChange = `${strPrice[i]}`;
-        }
+export const addCommaToPrice = (price) => {
+    const priceStr = price.toString();
+    let ret = "",
+      strCnt = 0;
+    for (let i = priceStr.length - 1; i >= 0; i--) {
+      strCnt += 1;
+      ret = `${priceStr[i]}${strCnt > 3 && strCnt % 3 === 1 ? "," : ""}${ret}`;
     }
-    return strChange;
-};
+    return ret;
+  };
