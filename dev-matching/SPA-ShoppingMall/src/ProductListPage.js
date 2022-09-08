f5c019d725f01164a8949cb85078cb6212fcee33
UihyunLee <88185154+leeuihyun@users.noreply.github.com>
2022-09-08 19:50:59 +0900

    Update ProductListPage.js
    productListPage

--- dev-matching/SPA-ShoppingMall/src/ProductListPage.js
+++ dev-matching/SPA-ShoppingMall/src/ProductListPage.js
@@ -1,49 +1,48 @@
-import Api from "./Api.js";
-import { getPrice } from "./util.js";
-
-export default function ProductListPage({ $app }) {
-    this.$target = document.createElement("div");
-    this.$target.className = "ProductListPage";
-
+import Api from './Api.js';
+import {addCommaToPrice} from './add.js';
+export default function ProductListPage({$app}){
+    this.state = {
+        items : [],
+    }
+    const api = new Api();
+    this.$target = document.createElement('div');
+    this.$target.className = 'ProductListPage';
     $app.appendChild(this.$target);
 
-    this.state = { products: [] };
-    this.setState = (nextState) => {
+    this.setState = nextState => {
         this.state = nextState;
         this.render();
-    };
-    const api = new Api();
+    }
+
     this.render = () => {
         this.$target.innerHTML = `
             <h1>상품목록</h1>
             <ul>
-            ${this.state.products
-                .map(
-                    (product) => `
-                <li class = "Product" data-id = "${product.id}">
-                    <img src = "${product.imageUrl}"/>
-                    <div class ="Product__info">
-                        <div>${product.name}</div>
-                        <div>${getPrice(product.price)}원~</div>
-                    </div>
-                </li>
-            `
-                )
-                .join("")}
+                ${this.state.items.map((item)=>`
+                    <li class ="Product" data-item-id = "${item.id}">
+                        <img src="${item.imageUrl}" data-item-id = "${item.id}"/>
+                        <div class = "Product__info" data-item-id = "${item.id}">
+                            <div>${item.name}</div>
+                            <div>${addCommaToPrice(item.price)}원~</div>
+                        </div>
+                    </li>
+                `).join('')}
             </ul>
-        `;
-    };
-    this.$target.querySelectorAll(".Product").forEach(($product) => {
-        $product.addEventListener("click", (e) => {
-            const { dataId } = e.target.dataset;
-            if (!dataId) return;
-            window.location.href = `/web/products/${dataId}`;
-        });
-    });
-
-    const fetchProducts = async () => {
-        const products = await api.getApi("/products");
-        this.setState({ products: products });
-    };
-    fetchProducts();
+        `
+        this.$target.querySelectorAll(".Product").forEach(($item)=>{
+            $item.addEventListener("click", (e) => {
+                const {itemId} = e.target.dataset; 
+                console.log(`click`);
+                location.href = `/web/products/${itemId}`;  
+            })
+        })
+    }
+    
+    const fetchItems = async() => {
+        const items = await api.get('/products');
+        this.setState({
+            items 
+        })
+    }
+    fetchItems();
 }
